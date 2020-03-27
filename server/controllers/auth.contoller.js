921a13772721a2ac07c1190fa59c7eac50e01b90
crusoe37 <crusoe37@gmail.com>
2020-03-27 21:03:22 +0200

    add api create and login logic

--- server/controllers/auth.contoller.js
+++ server/controllers/auth.contoller.js
@@ -1,6 +1,44 @@
-module.exports.login = (req, res) => {
+const bcrypt = require('bcrypt-nodejs')
+const jwt = require('jsonwebtoken')
+const keys = require('../keys')
+const User = require('../models/user.model')
 
+module.exports.login = async (req, res) => {
+  const candidate = await User.findOne({
+    login: req.body.login
+  })
+  if (candidate) {
+    const isPasswordCorrect = bcrypt.compareSync(req.body.password, candidate.password)
+
+    if (isPasswordCorrect) {
+      const token = jwt.sign({
+        login: candidate.login,
+        userId: candidate._id
+      }, keys.JWT, { expiresIn: 60 * 60 })
+      res.json({ token })
+    } else {
+      // Use 404 status in production
+      res.status(401).json({ message: 'Пароль не верен' })
+    }
+  } else {
+    res.status(404).json({
+      message: 'Пользователь не найден'
+    })
+  }
 }
 
-module.exports.createUser = (req, res) => {
+module.exports.createUser = async (req, res) => {
+  const candidate = await User.findOne({ login: req.body.login })
+
+  if (candidate) {
+    res.status(409).json({ message: 'Данный логин уже занят' })
+  } else {
+    const salt = bcrypt.genSaltSync(10)
+    const user = new User({
+      login: req.body.login,
+      password: bcrypt.hashSync(req.body.password, salt)
+    })
+    await user.save()
+    res.status(201).json(user)
+  }
 }
--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  MONGO_URI: 'mongodb+srv://crusoe37:calvin1509@awesome-clustername-mongodb-3qrep.mongodb.net/test?retryWrites=true&w=majority'
+  MONGO_URI: 'mongodb+srv://crusoe37:calvin1509@awesome-clustername-mongodb-3qrep.mongodb.net/test?retryWrites=true&w=majority',
+  JWT: 'dev-jwt-key'
 }

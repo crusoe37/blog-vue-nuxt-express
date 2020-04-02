e26c55ab0642c4e06f5dd54974e38520b4bbeacc
crusoe37 <crusoe37@gmail.com>
2020-04-02 20:48:47 +0300

    add passport logic

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
 const authRoutes = require('./routes/auth.routes')
+const postRoutes = require('./routes/post.routes')
+const passportStrategy = require('./middleware/passport-strategy')
 const keys = require('./keys')
 const app = express()
 
@@ -9,9 +12,13 @@ mongoose.connect(keys.MONGO_URI)
   .then(() => { console.log('MongoDB connected...') })
   .catch(error => console.error(error))
 
+app.use(passport.initialize())
+passport.use(passportStrategy)
+
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/post', postRoutes)
 
 module.exports = app
---
+++ server/routes/post.routes.js
@@ -0,0 +1,7 @@
+const passport = require('passport')
+const { Router } = require('express')
+const router = Router()
+
+// path /api/post
+
+module.exports = router

dc2e3f31bd59e8737c82617d02ce9d0200dd958b
crusoe37 <crusoe37@gmail.com>
2020-04-03 18:01:58 +0300

    add basic post routes and post controller

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,7 +1,46 @@
 const passport = require('passport')
 const { Router } = require('express')
+const ctr = require('../controllers/post.controller')
 const router = Router()
 
-// path /api/post
+// Admin
+// Path /api/auth/admin
+
+router.post(
+  '/admin',
+  passport.authenticate('jwt', { session: false }),
+  ctr.create
+)
+
+router.get(
+  '/admin',
+  passport.authenticate('jwt', { session: false }),
+  ctr.getAll
+)
+
+router.get(
+  '/admin/:id',
+  passport.authenticate('jwt', { session: false }),
+  ctr.getById
+)
+
+router.put(
+  '/admin/:id',
+  passport.authenticate('jwt', { session: false }),
+  ctr.update
+)
+
+router.delete(
+  '/admin/:id',
+  passport.authenticate('jwt', { session: false }),
+  ctr.remove
+)
+
+// Public
+// Path /api/post/
+
+router.get('/', ctr.getAll)
+router.get('/:id', ctr.getById)
+router.put('/:id', ctr.getView)
 
 module.exports = router

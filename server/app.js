9c496b1eb6da642cac5b53a7a3d540bb9ea49d01
crusoe37 <crusoe37@gmail.com>
2020-04-06 21:32:18 +0300

    add backend comments logic

--- server/app.js
+++ server/app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const passportStrategy = require('./middleware/passport-strategy')
 const authRoutes = require('./routes/auth.routes')
 const postRoutes = require('./routes/post.routes')
-const passportStrategy = require('./middleware/passport-strategy')
+const commentRoutes = require('./routes/comment.routes')
 const keys = require('./keys')
 const app = express()
 
@@ -20,5 +21,6 @@ app.use(bodyParser.json())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
+app.use('/api/comment', commentRoutes)
 
 module.exports = app
